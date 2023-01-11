3e069a37ad2b807d031620ee33da7dfd42253971
Nariman <Nariman079i>
2023-01-11 19:27:27 +0300

    edit

--- Etno/Etno/settings.py
+++ Etno/Etno/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = []
 # Default primary key field type
