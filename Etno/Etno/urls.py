b6eea831588575d7c050a51fae5ce8eb50426369
Nariman <Nariman079i>
2023-01-12 23:59:01 +0300

    edit site administartion

--- Etno/Etno/urls.py
+++ Etno/Etno/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from Etno import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('site/administration/qwerty654321/userroot/', admin.site.urls),
     path('',include('ture.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
