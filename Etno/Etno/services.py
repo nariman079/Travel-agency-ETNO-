71a63619a91ca1bbef3426b943afd0d6cbd82333
nariman079 <nariman079i@mail.ru>
2023-08-07 12:40:55 +0300

    commit

--- Etno/Etno/services.py
+++ Etno/Etno/services.py
@@ -2,20 +2,18 @@ from django.core.files import File
 from ture.models import Image
 from PIL import Image as IM
 
-def convert_images():
-    # Получение всех изображений, которые нужно конвертировать
+def convert_images(upload='img/'):
     images = Image.objects.all()
     
-    for instance in images[::2]:
+    for instance in images:
         path = instance.img.path
         if instance.img.path[-4:] != 'webp':
             im = IM.open(path).convert('RGB')
             extention = instance.img.path.rsplit(".", 2)[1]
             file_name = path.replace(extention, "webp")
-            file = file_name.split('img/')[1]
+            file = file_name.split(upload)[1]
             im.save(file_name, 'webp')
             instance.img.save(file,File(open(file_name,'rb')))
-            
             instance.save()
             print(instance.img)
             
