f902c1011c2ceb00592c58c25cf9a8d85fec75c4
nariman079 <nariman079i@mail.ru>
2023-08-07 12:06:27 +0300

    convert_img

--- Etno/Etno/services.py
+++ Etno/Etno/services.py
@@ -1,5 +1,4 @@
-from easy_thumbnails.files import get_thumbnailer
-
+from django.core.files import File
 from ture.models import Image
 from PIL import Image as IM
 
@@ -7,18 +6,19 @@ def convert_images():
     # Получение всех изображений, которые нужно конвертировать
     images = Image.objects.all()
     
-    for instance in images:
+    for instance in images[::2]:
         path = instance.img.path
         if instance.img.path[-4:] != 'webp':
             im = IM.open(path).convert('RGB')
             extention = instance.img.path.rsplit(".", 2)[1]
             file_name = path.replace(extention, "webp")
+            file = file_name.split('img/')[1]
             im.save(file_name, 'webp')
-            print(im, file_name, instance.img, instance.img.path)
-            print(im.name)
-            instance.img = file_name
+            instance.img.save(file,File(open(file_name,'rb')))
+            
             instance.save()
-            print(instance)
+            print(instance.img)
+            
 
 
 
